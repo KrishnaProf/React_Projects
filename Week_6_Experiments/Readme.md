6b58d7e791785dc17ababaa0b7af76b88d2395dd
Leela_Krishna <leela_krishna@dell.com>
2024-02-03 16:15:17 +0530

    Week 6 experiments updated

--- Week_6_Experiments/Readme.md
+++ Week_6_Experiments/Readme.md
@@ -1,1 +1,6 @@
+# Week-6-experiments
 
+Returning --- A component can only return a single top level xml.
+why? Makes it easy to do reconcilation
+
+re-rending
---
+++ Week_6_Experiments/src/App.jsx
@@ -0,0 +1,91 @@
+import { useState } from 'react'
+
+import './App.css'
+
+//let counter = 4;
+
+function App() {
+
+  // const [todos, setTodos] = useState([
+  //   {
+  //     id:1,
+  //     title: "Learn React",
+  //     description: "Learn React from scratch"
+  //   },
+  //   {
+  //     id:2,
+  //     title: "Learn Node",
+  //     description: "Learn Node from scratch"
+  //   },
+  //   {
+  //     id:3,
+  //     title: "Learn Express",
+  //     description: "Learn Express from scratch"
+  //   }
+  // ])
+
+  // function addTodo(){
+  //   setTodos([...todos, {
+  //     id: counter++,
+  //     title: "Learn Angular",
+  //     description: "Learn Angular from scratch"
+  //   }])
+    
+
+  // }
+
+
+  
+
+  return (
+
+      <>
+      {/* <button onClick={addTodo}>Add a todo</button>
+      {todos.map(todo => <Todos key={todo.id} title={todo.title} description={todo.description}></Todos>)}
+      <HeaderwithButton></HeaderwithButton>       
+        <Header title="Krishna2"></Header>
+        <Header title="Krishna3"></Header>
+        <Header title="Krishna4"></Header>
+        <Header title="Krishna5"></Header> */}
+
+      </>
+
+  )
+}
+
+// function Todos({title, description}){
+//   return <div>
+//     <h1>{title}</h1>
+//     <h5>{description}</h5>
+//   </div>
+// }
+
+
+
+// function HeaderwithButton(){
+//   const [title, setTitle] = useState("my name is Krishna")
+
+//   function Changename(){
+//     setTitle("My name is " + Math.random())
+//   }
+//   return (
+//     <>
+//         <button onClick={Changename}>Update the title</button>
+//         <Header title={title}></Header>
+
+//         </>
+
+//   )
+// }
+
+// function Header({title}){
+//   return <div>
+//     <h1>{title}</h1>
+//   </div>
+
+// }
+
+
+
+
+export default App
